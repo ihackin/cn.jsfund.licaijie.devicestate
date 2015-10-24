314cd64e6a822b1e6e33eb6615518c4d0dca7310
maning <maninghere@163.com>
2015-10-24 22:19:57 +0800

    update.

--- src/ios/CDVDeviceState.m
+++ src/ios/CDVDeviceState.m
@@ -10,11 +10,11 @@
 
 - (CDVPlugin*)initWithWebView:(UIWebView*)theWebView {
     self = [super init];
-
+    
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(applicationDidBecomeActive) name:UIApplicationDidBecomeActiveNotification object:nil];
-
+    
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(applicationDidEnterBackground) name:UIApplicationDidEnterBackgroundNotification object:nil];
-
+    
     return self;
 }
 
@@ -22,26 +22,24 @@
 - (void)start : (CDVInvokedUrlCommand *)command
 {
     self.callbackId = command.callbackId;
-    NSString * version =[[[NSBundle mainBundle]infoDictionary]objectForKey :@"CFBundleDisplayName"];
+    NSString * version =[[[NSBundle mainBundle]infoDictionary]objectForKey :@"start success"];
     CDVPluginResult * pluginResult =[CDVPluginResult resultWithStatus : CDVCommandStatus_OK messageAsString : version];
     [self.commandDelegate sendPluginResult : pluginResult callbackId : callbackId];
 }
 
 - (void) applicationDidBecomeActive {
     NSLog(@"trigger applicationDidBecomeActive.");
-
+    
     NSString* result = [NSString stringWithFormat:@"{state:%@}", @"resume"];
     CDVPluginResult * pluginResult =[CDVPluginResult resultWithStatus : CDVCommandStatus_OK messageAsString : result];
-    [self.commandDelegate sendPluginResult : pluginResult callbackId : callbackId];
-
+    [self.commandDelegate sendPluginResult : pluginResult callbackId : self.callbackId];
 }
 
 - (void) applicationDidEnterBackground {
     NSLog(@"trigger applicationDidEnterBackground.");
     NSString* result = [NSString stringWithFormat:@"{state:%@}", @"pause"];
     CDVPluginResult * pluginResult =[CDVPluginResult resultWithStatus : CDVCommandStatus_OK messageAsString : result];
-    [self.commandDelegate sendPluginResult : pluginResult callbackId : callbackId];
-
+    [self.commandDelegate sendPluginResult : pluginResult callbackId : self.callbackId];
 }
 
 @end
